0ee74879d77922ee714dc53c82c8a6d92a612099
Lucas Alexsandro <lf6645700@gmail.com>
2025-11-25 11:18:25 -0300

    Add files via upload

---
+++ Estruturas_condicionais.py
@@ -0,0 +1,13 @@
+MAIOR_IDADE =18
+idade = int(input("Digite sua idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("maior de idade, pode tirar a CNH")
+
+if idade < MAIOR_IDADE:
+    print("menor de idade, não pode tirar a CNH")
+
+if idade >= MAIOR_IDADE:
+    print("maior de idade, pode tirar a CNH")
+else:
+    print("menor de idade, não pode tirar a CNH")
--- operadores_logicos.py
+++ operadores_logicos.py
@@ -19,7 +19,14 @@ conta_especial=True
 
 
 
-saldo >= saque and saque <= limite or conta_especial and saldo >= saque
+exp = saldo >= saque and saque <= limite or conta_especial and saldo >= saque 
+print (exp)
 
-(saldo >= saque and saque <= limite) or (conta_especial and saldo >= saque)
+exp_2 = (saldo >= saque and saque <= limite) or (conta_especial and saldo >= saque)
+print (exp_2)
 
+conta_normal_com_saldo_suficiente = saldo >= saque and saque <= limite
+conta_especial_com_saldo_suficiente = conta_especial and saldo >= saque
+
+exp_3 = conta_normal_com_saldo_suficiente or conta_especial_com_saldo_suficiente
+print (exp_3)
